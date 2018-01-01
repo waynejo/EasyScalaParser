e4a350254b897a8cdfe04bc3fe11317e12be5424
Wayne <waynejo1024@gmail.com>
2018-01-01 14:46:32 +0900

    [Refactoring] remove unused brackets

--- src/test/scala/BasicSuite.scala
+++ src/test/scala/BasicSuite.scala
@@ -7,31 +7,31 @@ class BasicSuite extends FunSuite {
     test("simple 'and' success") {
         case class ParsingResult(v0: String, v1: String)
 
-        val parser = Parser.and("ab", "cd")({ case (v0, v1) =>
+        val parser = Parser.and("ab", "cd") { case (v0, v1) =>
             ParsingResult(v0, v1)
-        })
+        }
         assert(parser.parse("abcd").contains(ParsingResult("ab", "cd")))
     }
 
     test("simple 'and' fail") {
         case class ParsingResult(v0: String, v1: String)
 
-        val parser = Parser.and("ab", "cd")({ case (v0, v1) =>
+        val parser = Parser.and("ab", "cd") { case (v0, v1) =>
             ParsingResult(v0, v1)
-        })
+        }
         assert(parser.parse("abc").isEmpty)
     }
 
     test("stacked 'and' success") {
         case class ParsingResult(v0: String, v1: String, v2: String, v3: String)
 
-        val parser1 = Parser.and("cd", "ef")({ case (v0, v1) =>
+        val parser1 = Parser.and("cd", "ef") { case (v0, v1) =>
             ParsingResult("", v0, v1, "")
-        })
+        }
 
-        val parser2 = Parser.and("ab", parser1, "gh")({ case (v0, v1, v2) =>
+        val parser2 = Parser.and("ab", parser1, "gh") { case (v0, v1, v2) =>
             ParsingResult(v0, v1.v1, v1.v2, v2)
-        })
+        }
 
         assert(parser2.parse("abcdefgh").contains(ParsingResult("ab", "cd", "ef", "gh")))
     }
@@ -39,13 +39,13 @@ class BasicSuite extends FunSuite {
     test("stacked 'and' fail") {
         case class ParsingResult(v0: String, v1: String, v2: String, v3: String)
 
-        val parser1 = Parser.and("cd", "ef")({ case (v0, v1) =>
+        val parser1 = Parser.and("cd", "ef") { case (v0, v1) =>
             ParsingResult("", v0, v1, "")
-        })
+        }
 
-        val parser2 = Parser.and("ab", parser1, "gh")({ case (v0, v1, v2) =>
+        val parser2 = Parser.and("ab", parser1, "gh") { case (v0, v1, v2) =>
             ParsingResult(v0, v1.v1, v1.v2, v2)
-        })
+        }
 
         assert(parser2.parse("abcddfgh").isEmpty)
     }
@@ -53,12 +53,12 @@ class BasicSuite extends FunSuite {
     test("simple 'or'") {
         case class ParsingResult(v0: String)
 
-        val parser = Parser.or("ab", "cd")({
+        val parser = Parser.or("ab", "cd") {
             case Type1(v) =>
                 ParsingResult(v)
             case Type2(v) =>
                 ParsingResult(v)
-        })
+        }
 
         assert(parser.parse("ab").contains(ParsingResult("ab")))
         assert(parser.parse("cd").contains(ParsingResult("cd")))
