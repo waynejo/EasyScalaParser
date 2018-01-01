8aac5c88fd59de8b8692e9a7b02d2777006be030
Wayne <waynejo1024@gmail.com>
2018-01-01 19:01:54 +0900

    convert reference type's debugString to ""

--- src/main/scala/com/waynejo/parser/element/ReferenceParsingElement.scala
+++ src/main/scala/com/waynejo/parser/element/ReferenceParsingElement.scala
@@ -2,6 +2,6 @@ package com.waynejo.parser.element
 
 case class ReferenceParsingElement[A](reference: () => ParsingElement[A]) extends ParsingElement[A] {
     def debugString: String = {
-        reference().debugString
+        ""
     }
 }
