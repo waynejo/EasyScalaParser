e164a9b0be8512c34ea6b867f8346f5c06948104
Wayne <waynejo1024@gmail.com>
2018-01-18 02:17:14 +0900

    exclude terminal element in error message

--- src/main/scala/com/waynejo/parser/ParsingEngine.scala
+++ src/main/scala/com/waynejo/parser/ParsingEngine.scala
@@ -1,6 +1,6 @@
 package com.waynejo.parser
 
-import com.waynejo.parser.element._
+import com.waynejo.parser.element.{TerminalParsingElement, _}
 import com.waynejo.parser.injection.ParsingIgnore
 import com.waynejo.parser.parsing.{AndParsingEngine, BaseParsingEngine, OrParsingEngine}
 
@@ -9,13 +9,22 @@ object ParsingEngine {
     def _parse[A](parsingElement: ParsingElement[A], parsingContext: ParsingContext, parserStack: List[ParsingElement[_]]): Either[ParsingFailInfo, ParsingSuccessInfo[A]] = {
         val ignoredIndex = parsingContext.parsingInjection.ignore(parsingContext.text, parsingContext.textIndex)
         val nextContext = parsingContext.copy(textIndex = ignoredIndex)
-        val nextParsingStack = parsingElement :: parserStack
+
+        val nextParsingStack = updateParsingStack(parsingElement, parserStack)
 
         val parser = BaseParsingEngine.parse[A](nextContext, nextParsingStack)
             .orElse(AndParsingEngine.parse[A](nextContext, nextParsingStack))
             .orElse(OrParsingEngine.parse[A](nextContext, nextParsingStack))
 
         parser(parsingElement)
+    }
+
+    private def updateParsingStack[A](parsingElement: ParsingElement[A], parserStack: List[ParsingElement[_]]) = {
+        if (parsingElement.isInstanceOf[TerminalParsingElement]) {
+            parserStack
+        } else {
+            parsingElement :: parserStack
+        }
     }
 
     def parse[A](parsingElement: ParsingElement[A], text: String, parsingInjection: ParsingIgnore): Either[String, A] = {
