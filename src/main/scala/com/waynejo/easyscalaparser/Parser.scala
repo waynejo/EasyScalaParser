9328ea537d14d8a6452a0a437e1f06c76b029a8b
Wayne <waynejo1024@gmail.com>
2018-02-16 16:11:12 +0900

    change the number of parameters from 15 to 22.

--- src/main/scala/com/waynejo/easyscalaparser/Parser.scala
+++ src/main/scala/com/waynejo/easyscalaparser/Parser.scala
@@ -64,6 +64,34 @@ case class Parser(name: String) {
         AndParsingElement15[A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P](v0, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, reducer, name)
     }
 
+    def and[A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q](v0: ParsingElement[A], v1: ParsingElement[B], v2: ParsingElement[C], v3: ParsingElement[D], v4: ParsingElement[E], v5: ParsingElement[F], v6: ParsingElement[G], v7: ParsingElement[H], v8: ParsingElement[I], v9: ParsingElement[J], v10: ParsingElement[K], v11: ParsingElement[L], v12: ParsingElement[M], v13: ParsingElement[N], v14: ParsingElement[O], v15: ParsingElement[P])(reducer: ((A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P)) => Q): ParsingElement[Q] = {
+        AndParsingElement16[A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q](v0, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, reducer, name)
+    }
+
+    def and[A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R](v0: ParsingElement[A], v1: ParsingElement[B], v2: ParsingElement[C], v3: ParsingElement[D], v4: ParsingElement[E], v5: ParsingElement[F], v6: ParsingElement[G], v7: ParsingElement[H], v8: ParsingElement[I], v9: ParsingElement[J], v10: ParsingElement[K], v11: ParsingElement[L], v12: ParsingElement[M], v13: ParsingElement[N], v14: ParsingElement[O], v15: ParsingElement[P], v16: ParsingElement[Q])(reducer: ((A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q)) => R): ParsingElement[R] = {
+        AndParsingElement17[A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R](v0, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16, reducer, name)
+    }
+
+    def and[A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S](v0: ParsingElement[A], v1: ParsingElement[B], v2: ParsingElement[C], v3: ParsingElement[D], v4: ParsingElement[E], v5: ParsingElement[F], v6: ParsingElement[G], v7: ParsingElement[H], v8: ParsingElement[I], v9: ParsingElement[J], v10: ParsingElement[K], v11: ParsingElement[L], v12: ParsingElement[M], v13: ParsingElement[N], v14: ParsingElement[O], v15: ParsingElement[P], v16: ParsingElement[Q], v17: ParsingElement[R])(reducer: ((A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R)) => S): ParsingElement[S] = {
+        AndParsingElement18[A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S](v0, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16, v17, reducer, name)
+    }
+
+    def and[A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T](v0: ParsingElement[A], v1: ParsingElement[B], v2: ParsingElement[C], v3: ParsingElement[D], v4: ParsingElement[E], v5: ParsingElement[F], v6: ParsingElement[G], v7: ParsingElement[H], v8: ParsingElement[I], v9: ParsingElement[J], v10: ParsingElement[K], v11: ParsingElement[L], v12: ParsingElement[M], v13: ParsingElement[N], v14: ParsingElement[O], v15: ParsingElement[P], v16: ParsingElement[Q], v17: ParsingElement[R], v18: ParsingElement[S])(reducer: ((A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S)) => T): ParsingElement[T] = {
+        AndParsingElement19[A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T](v0, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16, v17, v18, reducer, name)
+    }
+
+    def and[A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U](v0: ParsingElement[A], v1: ParsingElement[B], v2: ParsingElement[C], v3: ParsingElement[D], v4: ParsingElement[E], v5: ParsingElement[F], v6: ParsingElement[G], v7: ParsingElement[H], v8: ParsingElement[I], v9: ParsingElement[J], v10: ParsingElement[K], v11: ParsingElement[L], v12: ParsingElement[M], v13: ParsingElement[N], v14: ParsingElement[O], v15: ParsingElement[P], v16: ParsingElement[Q], v17: ParsingElement[R], v18: ParsingElement[S], v19: ParsingElement[T])(reducer: ((A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T)) => U): ParsingElement[U] = {
+        AndParsingElement20[A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U](v0, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16, v17, v18, v19, reducer, name)
+    }
+
+    def and[A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V](v0: ParsingElement[A], v1: ParsingElement[B], v2: ParsingElement[C], v3: ParsingElement[D], v4: ParsingElement[E], v5: ParsingElement[F], v6: ParsingElement[G], v7: ParsingElement[H], v8: ParsingElement[I], v9: ParsingElement[J], v10: ParsingElement[K], v11: ParsingElement[L], v12: ParsingElement[M], v13: ParsingElement[N], v14: ParsingElement[O], v15: ParsingElement[P], v16: ParsingElement[Q], v17: ParsingElement[R], v18: ParsingElement[S], v19: ParsingElement[T], v20: ParsingElement[U])(reducer: ((A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U)) => V): ParsingElement[V] = {
+        AndParsingElement21[A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V](v0, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16, v17, v18, v19, v20, reducer, name)
+    }
+
+    def and[A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W](v0: ParsingElement[A], v1: ParsingElement[B], v2: ParsingElement[C], v3: ParsingElement[D], v4: ParsingElement[E], v5: ParsingElement[F], v6: ParsingElement[G], v7: ParsingElement[H], v8: ParsingElement[I], v9: ParsingElement[J], v10: ParsingElement[K], v11: ParsingElement[L], v12: ParsingElement[M], v13: ParsingElement[N], v14: ParsingElement[O], v15: ParsingElement[P], v16: ParsingElement[Q], v17: ParsingElement[R], v18: ParsingElement[S], v19: ParsingElement[T], v20: ParsingElement[U], v21: ParsingElement[V])(reducer: ((A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V)) => W): ParsingElement[W] = {
+        AndParsingElement22[A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W](v0, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16, v17, v18, v19, v20, v21, reducer, name)
+    }
+
     def or[A]: OrParsingElementBuilderHead[A] = {
         OrParsingElementBuilderHead[A](name)
     }
@@ -132,6 +160,34 @@ object Parser {
 
     def and[A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P](v0: ParsingElement[A], v1: ParsingElement[B], v2: ParsingElement[C], v3: ParsingElement[D], v4: ParsingElement[E], v5: ParsingElement[F], v6: ParsingElement[G], v7: ParsingElement[H], v8: ParsingElement[I], v9: ParsingElement[J], v10: ParsingElement[K], v11: ParsingElement[L], v12: ParsingElement[M], v13: ParsingElement[N], v14: ParsingElement[O])(reducer: ((A, B, C, D, E, F, G, H, I, J, K, L, M, N, O)) => P): ParsingElement[P] = {
         AndParsingElement15[A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P](v0, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, reducer)
+    }
+
+    def and[A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q](v0: ParsingElement[A], v1: ParsingElement[B], v2: ParsingElement[C], v3: ParsingElement[D], v4: ParsingElement[E], v5: ParsingElement[F], v6: ParsingElement[G], v7: ParsingElement[H], v8: ParsingElement[I], v9: ParsingElement[J], v10: ParsingElement[K], v11: ParsingElement[L], v12: ParsingElement[M], v13: ParsingElement[N], v14: ParsingElement[O], v15: ParsingElement[P])(reducer: ((A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P)) => Q): ParsingElement[Q] = {
+        AndParsingElement16[A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q](v0, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, reducer)
+    }
+
+    def and[A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R](v0: ParsingElement[A], v1: ParsingElement[B], v2: ParsingElement[C], v3: ParsingElement[D], v4: ParsingElement[E], v5: ParsingElement[F], v6: ParsingElement[G], v7: ParsingElement[H], v8: ParsingElement[I], v9: ParsingElement[J], v10: ParsingElement[K], v11: ParsingElement[L], v12: ParsingElement[M], v13: ParsingElement[N], v14: ParsingElement[O], v15: ParsingElement[P], v16: ParsingElement[Q])(reducer: ((A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q)) => R): ParsingElement[R] = {
+        AndParsingElement17[A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R](v0, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16, reducer)
+    }
+
+    def and[A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S](v0: ParsingElement[A], v1: ParsingElement[B], v2: ParsingElement[C], v3: ParsingElement[D], v4: ParsingElement[E], v5: ParsingElement[F], v6: ParsingElement[G], v7: ParsingElement[H], v8: ParsingElement[I], v9: ParsingElement[J], v10: ParsingElement[K], v11: ParsingElement[L], v12: ParsingElement[M], v13: ParsingElement[N], v14: ParsingElement[O], v15: ParsingElement[P], v16: ParsingElement[Q], v17: ParsingElement[R])(reducer: ((A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R)) => S): ParsingElement[S] = {
+        AndParsingElement18[A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S](v0, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16, v17, reducer)
+    }
+
+    def and[A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T](v0: ParsingElement[A], v1: ParsingElement[B], v2: ParsingElement[C], v3: ParsingElement[D], v4: ParsingElement[E], v5: ParsingElement[F], v6: ParsingElement[G], v7: ParsingElement[H], v8: ParsingElement[I], v9: ParsingElement[J], v10: ParsingElement[K], v11: ParsingElement[L], v12: ParsingElement[M], v13: ParsingElement[N], v14: ParsingElement[O], v15: ParsingElement[P], v16: ParsingElement[Q], v17: ParsingElement[R], v18: ParsingElement[S])(reducer: ((A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S)) => T): ParsingElement[T] = {
+        AndParsingElement19[A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T](v0, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16, v17, v18, reducer)
+    }
+
+    def and[A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U](v0: ParsingElement[A], v1: ParsingElement[B], v2: ParsingElement[C], v3: ParsingElement[D], v4: ParsingElement[E], v5: ParsingElement[F], v6: ParsingElement[G], v7: ParsingElement[H], v8: ParsingElement[I], v9: ParsingElement[J], v10: ParsingElement[K], v11: ParsingElement[L], v12: ParsingElement[M], v13: ParsingElement[N], v14: ParsingElement[O], v15: ParsingElement[P], v16: ParsingElement[Q], v17: ParsingElement[R], v18: ParsingElement[S], v19: ParsingElement[T])(reducer: ((A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T)) => U): ParsingElement[U] = {
+        AndParsingElement20[A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U](v0, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16, v17, v18, v19, reducer)
+    }
+
+    def and[A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V](v0: ParsingElement[A], v1: ParsingElement[B], v2: ParsingElement[C], v3: ParsingElement[D], v4: ParsingElement[E], v5: ParsingElement[F], v6: ParsingElement[G], v7: ParsingElement[H], v8: ParsingElement[I], v9: ParsingElement[J], v10: ParsingElement[K], v11: ParsingElement[L], v12: ParsingElement[M], v13: ParsingElement[N], v14: ParsingElement[O], v15: ParsingElement[P], v16: ParsingElement[Q], v17: ParsingElement[R], v18: ParsingElement[S], v19: ParsingElement[T], v20: ParsingElement[U])(reducer: ((A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U)) => V): ParsingElement[V] = {
+        AndParsingElement21[A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V](v0, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16, v17, v18, v19, v20, reducer)
+    }
+
+    def and[A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W](v0: ParsingElement[A], v1: ParsingElement[B], v2: ParsingElement[C], v3: ParsingElement[D], v4: ParsingElement[E], v5: ParsingElement[F], v6: ParsingElement[G], v7: ParsingElement[H], v8: ParsingElement[I], v9: ParsingElement[J], v10: ParsingElement[K], v11: ParsingElement[L], v12: ParsingElement[M], v13: ParsingElement[N], v14: ParsingElement[O], v15: ParsingElement[P], v16: ParsingElement[Q], v17: ParsingElement[R], v18: ParsingElement[S], v19: ParsingElement[T], v20: ParsingElement[U], v21: ParsingElement[V])(reducer: ((A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V)) => W): ParsingElement[W] = {
+        AndParsingElement22[A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W](v0, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16, v17, v18, v19, v20, v21, reducer)
     }
 
     def or[A]: OrParsingElementBuilderHead[A] = {
